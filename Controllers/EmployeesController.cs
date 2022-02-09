b1f32a6c1d1cbf4e511ac555783a6076f1daf7dc
Nipuna Bhanuka <nipunahettiwatta@gamil.com>
2022-02-09 13:26:32 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebApi.Controllers
+{
+    [Route("api/[controller]")] //class level attribute
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet] //method level attribute
+        public IActionResult Employees()
+        {
+            var emp =new string[] { "task1", "task2", "task3" };
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        public IActionResult NewEmployees()
+        {
+            return Ok();
+        }
+    }
+}
